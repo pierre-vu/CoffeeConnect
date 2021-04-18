a5a81e2d08cd650ca2651a30b8c56c72a705a8af
pierre-vu <78333849+pierre-vu@users.noreply.github.com>
2021-04-18 17:37:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-var express = require('express')
-var app = express()
-
-app.use(express.static(__dirname));
-
-var server = app.listen(process.env.PORT || 8000, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log('Express app listening at http://%s:%s', host, port)
-
-})
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('home.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(process.env.PORT || 8080);
