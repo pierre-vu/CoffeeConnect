bd0568802334b2b7d829023df87e4eacb80057a2
pierre-vu <78333849+pierre-vu@users.noreply.github.com>
2021-04-18 17:36:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('www'));
+app.use(express.static(__dirname));
 
 var server = app.listen(process.env.PORT || 8000, function () {
 
